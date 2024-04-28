346b7de37f78daa726fc09f1ecfe0346b3751e20
zhongguanfu <2193114950@qq.com>
2024-04-28 19:21:01 +0800

    4月28日上传

--- front/miniprogram/pages/BBS-detail/BBS-detail.js
+++ front/miniprogram/pages/BBS-detail/BBS-detail.js
@@ -6,15 +6,37 @@ Page({
    */
   data: {
     defaultData: {"title": "评论详情"},
+    comment:{},
+    media:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const that = this
+    wx.request({
+      url: 'http://47.99.147.35:8081/comment/getcommentbyid?commentId=' + options.commentId,
+      method: 'get',
+      success:function(res){
+        console.log(res)
+        that.setData({
+          comment:res.data.data.comment,
+          media:res.data.data.media
+        })
+      }
+    })
   },
 
+  previewImg: function (e) {
+    let index = e.target.dataset.id;
+    let _this = this;
+    console.log(e)
+    wx.previewImage({
+      current: _this.data.media[index].url,
+      urls: _this.data.media
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
